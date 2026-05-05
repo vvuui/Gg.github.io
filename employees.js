97d99cfbd353f3ab70317f46c33e55904d3320b6
vvuui <6jq5dkyrs5@privaterelay.appleid.com>
2026-05-05 05:57:52 +0300

    Add files via upload

---
+++ employees.js
@@ -0,0 +1,6 @@
+const employees = [
+  { id: "1120121379", name: "محمد" ,locationName: " مخيم 1" },
+  { id: "2020121379", name: "جعفر",locationName: " 21مخيم " },
+  { id: "3020121379", name: "احمد",locationName: " 251مخيم" },
+  { id: "4020121379", name: "حسين",locationName: " مخيم 221" }
+];
